d04995792f590e51a2a7ac3272f2dfdec6c9ccc5
Alexander <webfront22@gmail.com>
2023-03-21 22:13:06 +0700

    деструктуризация

--- js/render-preview.js
+++ js/render-preview.js
@@ -12,14 +12,14 @@ const similarPictures = getArrayCard();
 
 const pictureListFragment = document.createDocumentFragment();
 
-similarPictures.forEach((data) => {
+similarPictures.forEach(({ url, likes, comments }) => {
   const pictureElementClone = pictureTemplate.cloneNode(true);
   pictureElementClone
-    .querySelector('.picture__img').src = data.url;
+    .querySelector('.picture__img').src = url;
   pictureElementClone
-    .querySelector('.picture__likes').textContent = data.likes;
+    .querySelector('.picture__likes').textContent = likes;
   pictureElementClone
-    .querySelector('.picture__comments').textContent = data.comments.length;
+    .querySelector('.picture__comments').textContent = comments.length;
   pictureListFragment
     .append(pictureElementClone);
 });
